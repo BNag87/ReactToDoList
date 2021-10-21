0d19542de3c6a03ad070b95849335133b078f165
DrStupid87 <sharpshot101@hotmail.co.uk>
2021-10-21 16:43:50 +0100

    Added comments to code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
-import "./App.css";
+//↓↓↓ This is designed to allow the user to write a task, then show it on a browser window. All done with React
 
-//function to add a supplied input to a list
+import { useState } from "react"; //needed to use react components in functions
+import "./App.css"; //stylesheet
+
 
 const App = () => {
-  const [tasks, setTasks] = useState([])
-  const [input, setInput] = useState("")
+  
+  const [tasks, setTasks] = useState([]) //an array that will contain input elements (when a button is clicked)
+  const [input, setInput] = useState("") //gets input from an input element from html
  
+  //this function is called when a button is clicked. it creates a new array from a spread of the tasks array declared above
   const addHandler = () => {
-    let newArr = [...tasks]
-    newArr.push(input)
-    setTasks(newArr)
+    let newArr = [...tasks]   //spread the original array in to a new one...
+    newArr.push(input)        //...then push whatever the input is into the 'newArr' array...
+    setTasks(newArr)          //...finally, it updates the original array with the content of the new one
   }
 
+  //↓ this function is called when a button is clicked. it deletes a component from the render
+    //↓ index is passed down from the tasks array
   const deleteHandler = (index) => {
-    let newArr = [...tasks]
-    newArr.splice(index, 1)
-    setTasks(newArr)
-    setInput("")
+    let newArr = [...tasks]   //← spread tasks array in to a new array...
+    newArr.splice(index, 1)   //← remove the element at the index provided
+    setTasks(newArr)          //← update the original array with the modified one
+    setInput("")              //← clear the input so a new one can be written.
   }
   
 
@@ -27,18 +32,25 @@ const App = () => {
     <div>
       <h1>To-Do list</h1>
 
+    //↓ cant mutate an array in a useStatem so map the array (called task)
       {tasks.map((task, index) => {
        return(
-        <div key = {index} className = "taskWrapper">
-        <h2>{task}</h2>
-        <button onClick={() => deleteHandler(index)}>Done</button>
+        <div key = {index} className = "taskWrapper">                     //← a div wrapper to contain the component
+        <h2>{task}</h2>                                                   
+                                                                          //↑ pass the element called task
+        <button onClick={() => deleteHandler(index)}>Done</button>        
+                                                                          //↑ when this button is clicked, fire the deleteHandler function to remove the components
         </div>
 
        ) 
       })}
+      
       <input onChange={(event) => setInput(event.target.value)} type="text" id = "inputBox" />
+        //↑ when the input text is changed, fire the setinput function with the current value
 
       <button onClick={addHandler}>Add a Task</button>
+        //↑ when the button is clicked, fire the 'addHandler' function to add a task
+
     </div>
   )};
 
